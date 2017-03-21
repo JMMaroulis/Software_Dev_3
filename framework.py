5c8aa4dba1fd229271b050e85ebfb41d68bcfcf1
James Maroulis <jamesmmaroulis@hotmail.com>
2017-03-21 16:14:24 +0000

    Putting in original files

--- framework.py
+++ framework.py
@@ -7,126 +7,173 @@ from flask import Flask, request, render_template, redirect, g, send_from_direct
 
 # Create exportable app
 app = Flask(__name__)
+
+# define troop stats
 app.troops = {'Augment Gorilla': {'Move': 8, 'Fight': 3, 'Shoot': 0, 'Shield': 10, 'Morale': 2, 'Health': 8, 'Cost': 20,
                                   'Notes': 'Animal, Cannot carry treasure or items'},
+
               'Lackey': {'Move': 6, 'Fight': 2, 'Shoot': 0, 'Shield': 10, 'Morale': -1, 'Health': 10, 'Cost': 20,
                          'Notes': 'Melee Weapon'},
+
               'Security': {'Move': 6, 'Fight': 2, 'Shoot': 1, 'Shield': 12, 'Morale': 2, 'Health': 12, 'Cost': 80,
                            'Notes': 'Blaster, Blade'},
+
               'Engineer': {'Move': 4, 'Fight': 0, 'Shoot': 3, 'Shield': 12, 'Morale': 2, 'Health': 10, 'Cost': 60,
                            'Notes': 'Blaster, Repair Kit'},
+
               'Medic': {'Move': 5, 'Fight': 0, 'Shoot': 0, 'Shield': 12, 'Morale': 3, 'Health': 10, 'Cost': 50,
                         'Notes': 'Blade, Medkit'},
+
               'Commando': {'Move': 8, 'Fight': 4, 'Shoot': 0, 'Shield': 10, 'Morale': 4, 'Health': 12, 'Cost': 100,
                            'Notes': 'Stealth Suit, Blade, Needle Gun'},
-              'Combat Droid': {'Move': 3, 'Fight': 2, 'Shoot': 4, 'Shield': 14, 'Morale': 0, 'Health': 14, 'Cost': 150,
-                               'Notes': 'Mechanoid, Dual Blaster, Claws'},
-              }
 
-app.wizard = {
+              'Combat Droid': {'Move': 3, 'Fight': 2, 'Shoot': 4, 'Shield': 14, 'Morale': 0, 'Health': 14, 'Cost': 150,
+                               'Notes': 'Mechanoid, Dual Blaster, Claws'}}
+
+
+# define default captain stats
+app.captain = {
     'Captain': {'Move': 5, 'Fight': 2, 'Shoot': 2, 'Shield': 12, 'Morale': 4, 'Health': 12, 'Cost': 0, 'Skillset': [],
                 'Specialism': None, 'Items': [], 'Experience': 0}}
 
-app.apprentice = {'Ensign': {'Move': 7, 'Fight': 0, 'Shoot': -1, 'Shield': 10, 'Morale': 2, 'Health': 8, 'Skillset': [],
+# define default ensign stats
+app.ensign = {'Ensign': {'Move': 7, 'Fight': 0, 'Shoot': -1, 'Shield': 10, 'Morale': 2, 'Health': 8, 'Skillset': [],
                              'Specialism': None, 'Cost': 250, 'Items': [], 'Experience': 0}}
 
+# specialisms list
 app.specialisms = ['Engineering', 'Psychology', 'Marksman', 'Tactics', 'Melee', 'Defence']
 
+# dictionary mapping skills to specialisms
 app.skillsets = {'Engineering': ['Repair', 'Sabotage', 'Augment'], 'Psychology': ['Bolster', 'Terror', 'Counter'],
                  'Marksman': ['Aim', 'Pierce', 'Reload'], 'Tactics': ['Squad', 'Ambush', 'Surround'],
                  'Melee': ['Block', 'Risposte', 'Dual'], 'Defence': ['Shield', 'Sacrifice', 'Resolute']}
 
+# available items list
 app.weapon = ['Blaster', 'Needle Gun', 'Blade', 'Cannon', 'Whip']
 
+# item costs dictionary
 app.cost = {'Blaster': 5, 'Needle Gun': 12, 'Blade': 3, 'Cannon': 15, 'Whip': 5}
 
 
 @app.route('/', methods=['GET'])
 
 
+# runs welcome page
 def welcome_page():
     return app.send_static_file('index.html'), httpcodes.OK
 
 
+# attempts to figure out sum cost of warband
 def sumband(createdband):
     total = 0;
+
+    # cost for captain items
     for item in createdband['Captain']['Items']:
         total = total + app.cost[item]
+
+    # cost for hiring ensign
     if 'Ensign' in createdband.keys():
         total = total + 250
-        for item in createdband['Captain']['Items']:
+
+        # cost for ensign items
+        for item in createdband['Ensign']['Items']:
             total = total + app.cost[item]
+
+    # cost for hired troops
     for troop in createdband['Troops']:
         total = total + app.troops[troop]['Cost']
+
     return total
 
 
+# Band validation on creation
 def validate_band(createdband):
     ''' Need to write the validation '''
     return True
 
 
+# band validation on edit
 def validate_band(oldband, newband):
     return True
 
 
+# attempts warband creation
 @app.route('/new', methods=['GET', 'POST'])
 def new_warband():
 
     if request.method == 'GET':
-        return render_template('blankband.html', people=app.troops, wizard=app.wizard, apprentice=app.apprentice,
+        return render_template('blankband.html', people=app.troops, captain=app.captain, ensign=app.ensign,
                                specs=app.specialisms, skills=app.skillsets, weaps=app.weapon), httpcodes.OK
-
+    #
     if request.method == 'POST':
+
+        # pull various flask forms
         bandname = request.form['bandname']
         capspec = request.form['capspec']
         capskill = request.form['capskill']
         capweap = request.form['capweap']
         troops = json.loads(request.form['troops'])
+
+        # create dictionary to hold band details
         createdband = dict()
+
+        # put captain dictionaries in band dictionary
         createdband['Name'] = bandname
-        createdband['Captain'] = dict(app.wizard['Captain'])
+        createdband['Captain'] = dict(app.captain['Captain'])
         createdband['Captain']['Specialism'] = capspec
         createdband['Captain']['Skillset'].append(capskill)
         createdband['Captain']['Items'].append(capweap)
 
+        # if ensign exists, put ensign dictionaries in band dictionary
         if 'hasensign' in request.form.keys():
+
+            # pull dictionaries
             ensspec = request.form['ensspec']
             ensskill = request.form['ensskill']
             ensweap = request.form['ensweap']
-            createdband['Ensign'] = dict(app.apprentice['Ensign'])
+
+            # place dictionaries
+            createdband['Ensign'] = dict(app.ensign['Ensign'])
             createdband['Ensign']['Specialism'] = ensspec
             createdband['Ensign']['Skillset'].append(ensskill)
             createdband['Ensign']['Items'].append(ensweap)
+
+        # create empty troop dictionary
         createdband['Troops'] = []
 
+        # populate troop dictionary with troops
         for item in troops:
             if item != "Empty":
                 createdband['Troops'].append(item)
 
+        # too many crew check
         if len(createdband['Troops']) > 9:
-            return render_template('blankband.html', people=app.troops, wizard=app.wizard, apprentice=app.apprentice,
-                                   specs=app.specialisms, skills=app.skillsets, weaps=app.weapon), httpcodes.BAD_REQUEST
+            return render_template('blankband.html', people=app.troops, captain=app.captain, ensign=app.ensign,
+                                   specs=app.specialisms, skills=app.skillsets, weaps=app.weapon), httpcodes.OK
         createdband['Treasury'] = 500 - sumband(createdband)
 
+        # not enough money check
         if createdband['Treasury'] < 0:
-            return render_template('blankband.html', people=app.troops, wizard=app.wizard, apprentice=app.apprentice,
+            return render_template('blankband.html', people=app.troops, captain=app.captain, ensign=app.ensign,
                                    specs=app.specialisms, skills=app.skillsets, weaps=app.weapon), httpcodes.BAD_REQUEST
         pickle.dump(createdband,
                     open(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands"), bandname),
                          "wb"))
 
         return render_template('blankband.html', specs=app.specialisms, skills=app.skillsets, people=app.troops,
-                               wizard=app.wizard, apprentice=app.apprentice), httpcodes.CREATED
+                               captain=app.captain, ensign=app.ensign), httpcodes.CREATED
 
 
 @app.route('/edit', methods=['GET'])
 def edit_warband():
+    # create band list
     if os.path.isdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands")):
         bands = os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands"))
+    # if there are no bands, make null bands object
     else:
         os.mkdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands"))
         bands = None
+
     if request.method == 'GET':
         return render_template('bandlist.html', bands=bands), httpcodes.OK
 
@@ -135,90 +182,114 @@ def edit_warband():
 def edit_given_warband(band):
     loadedband = pickle.load(
         open(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands"), band), "rb"))
+
     if request.method == 'GET':
-        return render_template('editband.html', band=loadedband, people=app.troops, wizard=app.wizard,
-                               apprentice=app.apprentice, specs=app.specialisms, skills=app.skillsets,
+        return render_template('editband.html', band=loadedband, people=app.troops, captain=app.captain,
+                               ensign=app.ensign, specs=app.specialisms, skills=app.skillsets,
                                weaps=app.weapon), httpcodes.OK
+
     if request.method == 'POST':
 
+        # pull captain stats
         bandname = request.form['bandname']
-        capspec = request.form['capspec']
+        captain_speciality = request.form['captain_speciality']
         # capskill = request.form['capskill']
         skills = json.loads(request.form['capskill'])
         capweap = request.form['capweap']
         troops = json.loads(request.form['troops'])
-        capmov = request.form['capmove']
-        capfig = request.form['capfight']
-        capsho = request.form['capshoot']
-        capshi = request.form['capshield']
-        capmor = request.form['capmorale']
-        caphea = request.form['caphealth']
-        capexp = request.form['capexperience']
+        captain_move = request.form['capmove']
+        captain_fight = request.form['capfight']
+        captain_shoot = request.form['capshoot']
+        captain_shield = request.form['capshield']
+        captain_morale = request.form['capmorale']
+        captain_health = request.form['caphealth']
+        captain_experience = request.form['capexperience']
         createdband = dict()
         createdband['Name'] = bandname
-        createdband['Captain'] = dict(app.wizard['Captain'])
-        createdband['Captain']['Specialism'] = capspec
+        createdband['Captain'] = dict(app.captain['Captain'])
+        createdband['Captain']['Specialism'] = captain_speciality
         createdband['Captain']['Skillset'].extend(skills)
         createdband['Captain']['Items'].append(capweap)
-        createdband['Captain']['Move'] = capmov
-        createdband['Captain']['Fight'] = capfig
-        createdband['Captain']['Shoot'] = capsho
-        createdband['Captain']['Shield'] = capshi
-        createdband['Captain']['Morale'] = capmor
-        createdband['Captain']['Health'] = caphea
-        createdband['Captain']['Experience'] = capexp
+        createdband['Captain']['Move'] = captain_move
+        createdband['Captain']['Fight'] = captain_fight
+        createdband['Captain']['Shoot'] = captain_shoot
+        createdband['Captain']['Shield'] = captain_shield
+        createdband['Captain']['Morale'] = captain_morale
+        createdband['Captain']['Health'] = captain_health
+        createdband['Captain']['Experience'] = captain_experience
+
+        # if ensign exists, pull ensign stats
         if 'hasensign' in request.form.keys():
             ensspec = request.form['ensspec']
             # ensskill = request.form['ensskill']
-            eskills = json.loads(request.form['ensskill'])
-            ensmov = request.form['ensmove']
-            ensfig = request.form['ensfight']
-            enssho = request.form['ensshoot']
-            ensshi = request.form['ensshield']
-            ensmor = request.form['ensmorale']
-            enshea = request.form['enshealth']
-            ensexp = request.form['ensexperience']
+            ensign_skill = json.loads(request.form['ensign_skill'])
+            ensign_move = request.form['ensmove']
+            ensign_fight = request.form['ensfight']
+            ensign_shoot = request.form['ensshoot']
+            ensign_shield = request.form['ensshield']
+            ensign_morale = request.form['ensmorale']
+            ensign_health = request.form['enshealth']
+            ensign_experience = request.form['ensexperience']
             ensweap = request.form['ensweap']
-            createdband['Ensign'] = dict(app.apprentice['Ensign'])
+            createdband['Ensign'] = dict(app.ensign['Ensign'])
             createdband['Ensign']['Specialism'] = ensspec
-            createdband['Ensign']['Skillset'].extend(eskills)
+            createdband['Ensign']['Skillset'].extend(ensign_skill)
             createdband['Ensign']['Items'].append(ensweap)
-            createdband['Ensign']['Move'] = ensmov
-            createdband['Ensign']['Fight'] = ensfig
-            createdband['Ensign']['Shoot'] = enssho
-            createdband['Ensign']['Shield'] = ensshi
-            createdband['Ensign']['Morale'] = ensmor
-            createdband['Ensign']['Health'] = enshea
-            createdband['Ensign']['Experience'] = ensexp
+            createdband['Ensign']['Move'] = ensign_move
+            createdband['Ensign']['Fight'] = ensign_fight
+            createdband['Ensign']['Shoot'] = ensign_shoot
+            createdband['Ensign']['Shield'] = ensign_shield
+            createdband['Ensign']['Morale'] = ensign_morale
+            createdband['Ensign']['Health'] = ensign_health
+            createdband['Ensign']['Experience'] = ensign_experience
+
+        # create troops list
         createdband['Troops'] = []
-        for item in troops:
-            if item != "Empty":
-                createdband['Troops'].append(item)
+
+        # populate troop list
+        for troop in troops:
+            if troop != "Empty":
+                createdband['Troops'].append(troop)
+
+        # render blank band if too many troops?
+        # sounds like a bug to me
         if len(createdband['Troops']) > 9:
-            return render_template('blankband.html', people=app.troops, wizard=app.wizard, apprentice=app.apprentice,
+            return render_template('blankband.html', people=app.troops, captain=app.captain, ensign=app.ensign,
                                    specs=app.specialisms, skills=app.skillsets, weaps=app.weapon), httpcodes.OK
+
+        # if band passes validation
         if validate_band(loadedband, createdband):
+
+            # set cash at 500 - cost
             createdband['Treasury'] = 500 - sumband(createdband)
             pickle.dump(createdband,
                         open(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands"), bandname),
                              "wb"))
-            return render_template('editband.html', band=createdband, people=app.troops, wizard=app.wizard,
-                                   apprentice=app.apprentice, specs=app.specialisms, skills=app.skillsets,
+            return render_template('editband.html', band=createdband, people=app.troops, captain=app.captain,
+                                   ensign=app.ensign, specs=app.specialisms, skills=app.skillsets,
                                    weaps=app.weapon), httpcodes.OK
+
+        # if band fails validation
         else:
-            return render_template('editband.html', band=loadedband, people=app.troops, wizard=app.wizard,
-                                   apprentice=app.apprentice, specs=app.specialisms, skills=app.skillsets,
+            return render_template('editband.html', band=loadedband, people=app.troops, captain=app.captain,
+                                   ensign=app.ensign, specs=app.specialisms, skills=app.skillsets,
                                    weaps=app.weapon), httpcodes.BAD_REQUEST
 
 
 @app.route('/delete/<band>', methods=['GET'])
 def delete_given_warband(band):
+    # deletes band file
     os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands", band))
+
+    # create band list
     if os.path.isdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands")):
         bands = os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands"))
+
+    # if there are no bands, make a null bands object
     else:
         os.mkdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bands"))
         bands = None
+
     if request.method == 'GET':
         return render_template('bandlist.html', bands=bands), httpcodes.OK
 
